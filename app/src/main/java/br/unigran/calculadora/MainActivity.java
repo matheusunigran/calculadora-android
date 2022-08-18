58a129e0dc752a3474f332f524cb9719339d290e
matheus ribeiro santana <802.243@alunos.unigran.br>
2022-08-18 05:21:12 -0400

    Verificando se não existe campos vazios para não travar o app

--- app/src/main/java/br/unigran/calculadora/MainActivity.java
+++ app/src/main/java/br/unigran/calculadora/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText nmr2;
     private TextView resultado;
 
+    public double valor1, valor2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,37 +22,40 @@ public class MainActivity extends AppCompatActivity {
         nmr1 = findViewById(R.id.num1);
         nmr2 = findViewById(R.id.num2);
         resultado = findViewById(R.id.textView4);
-    }
-    public void somar(View view){
-        double valor1 = Double.parseDouble(nmr1.getText().toString());
-        double valor2 = Double.parseDouble(nmr2.getText().toString());
 
-        resultado.setText(String.valueOf(valor1+valor2));
     }
-    public void subtrair(View view){
-        double valor1 = Double.parseDouble(nmr1.getText().toString());
-        double valor2 = Double.parseDouble(nmr2.getText().toString());
-
-        resultado.setText(String.valueOf(valor1-valor2));
-    }
-    public void multiplicar(View view){
-        double valor1 = Double.parseDouble(nmr1.getText().toString());
-        double valor2 = Double.parseDouble(nmr2.getText().toString());
-
-        resultado.setText(String.valueOf(valor1*valor2));
-    }
-    public void dividir(View view){
-        double valor1 = Double.parseDouble(nmr1.getText().toString());
-        double valor2 = Double.parseDouble(nmr2.getText().toString());
-
-        if(valor2 != 0)
-            resultado.setText(String.valueOf(valor1/valor2));
-        else{
-            resultado.setText("Não é possível resolver uma divisão por 0");
+    public boolean verificar(){
+        String s1 = nmr1.getText().toString();
+        String s2 = nmr2.getText().toString();
+        if ((s1.equals(null) || s2.equals(null))
+                || (s1.equals("") || s2.equals(""))) {
+            resultado.setText("Informe os números corretamente");
+            return false;
+        } else{
+            valor1 = Double.parseDouble(nmr1.getText().toString());
+            valor2 = Double.parseDouble(nmr2.getText().toString());
+            return true;
         }
     }
-
-
-
-
+    public void somar(View view){
+        if(verificar())
+            resultado.setText(String.valueOf(valor1 + valor2));
+    }
+    public void subtrair(View view){
+        if(verificar())
+            resultado.setText(String.valueOf(valor1-valor2));
+    }
+    public void multiplicar(View view){
+        if(verificar())
+            resultado.setText(String.valueOf(valor1*valor2));
+    }
+    public void dividir(View view){
+        if(verificar()){
+            if(valor2 != 0)
+                resultado.setText(String.valueOf(valor1/valor2));
+            else{
+                resultado.setText("Não é possível resolver uma divisão por 0");
+            }
+        }
+    }
 }
