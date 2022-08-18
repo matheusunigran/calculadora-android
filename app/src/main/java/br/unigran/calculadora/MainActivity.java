c3b75d397c99173c5fc145be285cd1f94df99bc6
matheus ribeiro santana <802.243@alunos.unigran.br>
2022-08-18 03:35:45 -0400

    Feito até a operação de soma

--- app/src/main/java/br/unigran/calculadora/MainActivity.java
+++ app/src/main/java/br/unigran/calculadora/MainActivity.java
@@ -2,12 +2,29 @@ package br.unigran.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nmr1;
+    private EditText nmr2;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nmr1 = findViewById(R.id.num1);
+        nmr2 = findViewById(R.id.num2);
+        resultado = findViewById(R.id.textView4);
+    }
+    public void somar(View view){
+        double valor1 = Double.parseDouble(nmr1.getText().toString());
+        double valor2 = Double.parseDouble(nmr2.getText().toString());
+
+        resultado.setText(String.valueOf(valor1+valor2));
     }
 }
